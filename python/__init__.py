9fc0bf86a47158202dd7255b04ad2c971b414b45
Joosep <joosep.pata@gmail.com>
2014-11-14 13:07:55 +0100

    first commit

---
+++ python/__init__.py
@@ -0,0 +1,3 @@
+#Automatically created by SCRAM
+import os
+__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/TTH/MEStudiesJP/',1)[0])+'/cfipython/osx108_amd64_gcc481/TTH/MEStudiesJP')
---
+++ python/cutflow.py
@@ -0,0 +1,40 @@
+import FWCore.ParameterSet.Types  as CfgTypes
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("MEAnalysisNew")
+
+process.fwliteInput = cms.PSet(
+	samples = cms.VPSet([
+		cms.PSet(
+			fileName=cms.string("/Users/joosep/Documents/tth/step2/nov11/tthbb.root"),
+            nickName=cms.string("tthbb_13TeV"),
+			type=cms.int32(3)
+		),
+        cms.PSet(
+            fileName=cms.string("/Users/joosep/Documents/tth/step2/nov11/ttjets.root"),
+            nickName=cms.string("ttjets_13TeV"),
+            type=cms.int32(3)
+        ),
+        cms.PSet(
+            fileName=cms.string("/Users/joosep/Documents/tth/Apr23_2014/MEAnalysisNew_all_rec_std_TTH125.root"),
+            nickName=cms.string("tthbb_8TeV_ME"),
+            type=cms.int32(1)
+        ),
+        cms.PSet(
+            fileName=cms.string("/Users/joosep/Documents/tth/Apr23_2014/MEAnalysisNew_all_rec_std_TTJets_nC.root"),
+            nickName=cms.string("ttjets_8TeV_ME"),
+            type=cms.int32(1)
+        ),
+        cms.PSet(
+            fileName=cms.string("/Users/joosep/Documents/tth/MEAnalysisNew_all_ntuplizeAll_v3_rec_std_TTH125.root"),
+            nickName=cms.string("tthbb_8TeV_noME"),
+            type=cms.int32(0)
+        ),
+        cms.PSet(
+            fileName=cms.string("/Users/joosep/Documents/tth/MEAnalysisNew_all_ntuplizeAll_v3_rec_std_TTJets_nC.root"),
+            nickName=cms.string("ttjets_8TeV_noME"),
+            type=cms.int32(0)
+        ),
+    ]),
+    evLimits=cms.vint32(0, 15000000)
+)
